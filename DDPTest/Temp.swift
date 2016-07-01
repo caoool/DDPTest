3e3006d2767ed705e3c50ddc14e49f79ff4ceeee
Lu Cao <caoool.link@icloud.com>
2016-07-02 03:03:31 +0800

    Add events.feeds subscribe

---
+++ DDPTest/Temp.swift
@@ -0,0 +1,16 @@
+//
+//  Temp.swift
+//  DDPTest
+//
+//  Created by Lu Cao on 6/30/16.
+//  Copyright © 2016 Lu Cao. All rights reserved.
+//
+
+import Foundation
+import SwiftDDP
+
+class Temp: MeteorDocument {
+    
+    var collection:String = "temps"
+    var summary:String?
+}
